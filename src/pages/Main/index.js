63f4b79f3d2db06c694e5706e6865a806ac14808
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-05-20 18:00:43 -0300

    Added form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-// import { Container } from './styles';
+import { Container, Title, Form, Input, Submit } from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <Container>
+      <Title>Repositories</Title>
+      <Form>
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Browse repo..."
+        />
+        <Submit onPress={() => {}}>
+          <Icon name="add" size={22} color="#FFF" />
+        </Submit>
+      </Form>
+    </Container>
+  );
 }
